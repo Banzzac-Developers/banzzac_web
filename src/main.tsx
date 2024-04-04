8ab850b951f22f5579fb4dd6d390f7ac4ce04ebc
hyojinLeee-git <splato8899@gmail.com>
2024-04-04 15:08:42 +0900

    feat: remove react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -10,9 +9,7 @@ if (import.meta.env.VITE_NODE_ENV === "mocking") {
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>,
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
 );
