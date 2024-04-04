a74241fc0b180ef6ef4eabd23de47b4dad86086b
hyojinLeee-git <splato8899@gmail.com>
2024-04-04 15:09:02 +0900

    feat: add reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./reset.css";
 import "./index.css";
 import { worker } from "./mocks/browser.ts";
 import { RecoilRoot } from "recoil";
