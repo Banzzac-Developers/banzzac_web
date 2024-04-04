f0ac7fcbe154bfc2fded77d6f389dcd8b9c3bfe4
hyojinLeee-git <splato8899@gmail.com>
2024-04-05 00:30:45 +0900

    feat: add RoundHeader component

---
+++ src/layouts/RoundHeader.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export default function RoundHeader() {
+  return <Header>header</Header>;
+}
+
+const Header = styled.header`
+  border-bottom: 1px solid #212121;
+  border-radius: 0px 0px 16px 16px;
+  box-shadow: 0px 4px 4px 0px #00000026;
+  padding: 24px;
+`;
