1bc8e699ef802a9423f7fa3189fd7d37db998d19
hyojinLeee-git <splato8899@gmail.com>
2024-04-04 16:31:13 +0900

    feat: 폰트 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { RecoilRoot } from "recoil";
 
 import "@styles/reset.css";
 import "@styles/index.css";
+import "@/assets/fonts/web/static/pretendard.css";
+import "@/assets/fonts/web/static/pretendard-subset.css";
+import "@/assets/fonts/web/variable/pretendardvariable.css";
 import App from "@/App.tsx";
 import { worker } from "@/mocks/browser.ts";
 
