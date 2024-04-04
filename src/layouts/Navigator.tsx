d438c0d9fdd7278489e5a0a00794504e48d7b1bd
hyojinLeee-git <splato8899@gmail.com>
2024-04-04 18:52:38 +0900

    feat: navigator 구현

--- src/layouts/Navigator.tsx
+++ src/layouts/Navigator.tsx
@@ -1,3 +1,7 @@
+import styled from "@emotion/styled";
+import { navList } from "@models/index";
+import { Link } from "react-router-dom";
+
 type Props = {
   children: React.ReactNode;
 };
@@ -6,7 +10,45 @@ export default function Navigator({ children }: Props) {
   return (
     <>
       {children}
-      <nav>navigator</nav>
+      <Container>
+        <ul>
+          {navList.map((navItem) => (
+            <li key={navItem.name}>
+              <Link to={navItem.route}>{navItem.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
     </>
   );
 }
+
+const Container = styled.nav`
+  border: 1px solid #212121;
+  padding: 5px 22px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 56px;
+  position: absolute;
+  width: calc(100% - (24px * 2));
+  left: 24px;
+  bottom: 7px;
+
+  ul {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    li {
+      width: 50px;
+      height: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 10px;
+      font-weight: 600;
+      color: #212121;
+    }
+  }
+`;
---
+++ src/models/index.ts
@@ -0,0 +1,27 @@
+export const navList = [
+  {
+    route: "/friends",
+    icon: "",
+    name: "친구",
+  },
+  {
+    route: "/chat",
+    icon: "",
+    name: "채팅",
+  },
+  {
+    route: "/search",
+    icon: "",
+    name: "탐색",
+  },
+  {
+    route: "/matching",
+    icon: "",
+    name: "매칭",
+  },
+  {
+    route: "/profile",
+    icon: "",
+    name: "내 프로필",
+  },
+];
