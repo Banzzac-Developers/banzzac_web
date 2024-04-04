143a7f99fc7e1b4d7cb862c48c3155df9155b73d
hyojinLeee-git <splato8899@gmail.com>
2024-04-04 18:04:44 +0900

    feat: 페이지 세팅 및 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import Chat from "@pages/Chat";
+import ChatRoom from "@pages/Chat/ChatRoom";
+import NotFound from "@pages/Error/NotFound";
+import Friends from "@pages/Friends";
+import Login from "@pages/Login";
+import Matching from "@pages/Matching";
+import Profile from "@pages/Profile";
+import ProfileEdit from "@pages/Profile/Edit";
+import Search from "@pages/Search";
+import Signup from "@pages/Signup";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<NotFound />}>
+      <Route path="/" element={<div>splash</div>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="/chat/:id" element={<ChatRoom />} />
+      <Route path="/friends" element={<Friends />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/matching" element={<Matching />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/edit" element={<ProfileEdit />} />
+    </Route>,
+  ),
+);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- src/layouts/Navigator.tsx
+++ src/layouts/Navigator.tsx
@@ -1,3 +1,12 @@
-export default function Navigator() {
-  return <nav>navigator</nav>;
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Navigator({ children }: Props) {
+  return (
+    <>
+      {children}
+      <nav>navigator</nav>
+    </>
+  );
 }
