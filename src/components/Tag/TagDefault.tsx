d0bef0fb417ddcdc8ed36615477cb3989f6cf058
hyojinLeee-git <splato8899@gmail.com>
2024-04-05 21:39:28 +0900

    feat: tag default component 추가

---
+++ src/components/Tag/TagDefault.tsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+type Props = {
+  txt: string;
+  bold?: boolean;
+};
+
+export default function TagDefault({ txt, bold = false }: Props) {
+  return <Container bold={bold}>{txt}</Container>;
+}
+
+const Container = styled.div<{ bold: boolean }>`
+  background-color: #eeeeee;
+  color: #424242;
+  font-weight: ${({ bold }) => (bold ? 700 : 500)};
+  width: fit-content;
+  padding: 3px 6px;
+  border-radius: 40px;
+  line-height: ${({ bold }) => (bold ? "20px" : "22px")};
+  font-size: 14px;
+`;
