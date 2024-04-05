8ce40471b57445663f65acb05589d1e69053de20
hyojinLeee-git <splato8899@gmail.com>
2024-04-05 21:39:43 +0900

    feat: 둥근 헤더 디자인 구현

--- src/layouts/RoundHeader.tsx
+++ src/layouts/RoundHeader.tsx
@@ -1,7 +1,24 @@
+import SvgSelector from "@components/Svg/SvgSelector";
+import TagDefault from "@components/Tag/TagDefault";
 import styled from "@emotion/styled";
 
 export default function RoundHeader() {
-  return <Header>header</Header>;
+  return (
+    <Header>
+      <Wrapper>
+        <SvgSelector svg={"dogFace"} width={24} height={24} stroke="#212121" />
+        <div>
+          <TagDefault txt="적용된 필터 없음" />
+        </div>
+      </Wrapper>
+      <Wrapper>
+        <SvgSelector svg={"face"} width={24} height={24} stroke="#212121" />
+        <div>
+          <TagDefault txt="적용된 필터 없음" />
+        </div>
+      </Wrapper>
+    </Header>
+  );
 }
 
 const Header = styled.header`
@@ -9,4 +26,14 @@ const Header = styled.header`
   border-radius: 0px 0px 16px 16px;
   box-shadow: 0px 4px 4px 0px #00000026;
   padding: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 8px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 4px;
 `;
