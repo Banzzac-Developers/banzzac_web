7399acb7d5692309962cbcf647a43525b0daf380
Ryu_woong <jin02014@naver.com>
2024-04-07 19:03:12 +0900

    fix: 중앙정렬

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,4 +1,5 @@
-import { Box, Slider } from "@mui/material"
+import styled from "@emotion/styled";
+import { Slider } from "@mui/material"
 import { useState } from "react";
 
 function valuetext(value: number) {
@@ -13,9 +14,9 @@ type Props ={
 }
 
 export default function MultiSlider( {width, max=100, min=0} :Props ){
-    const [value1, setValue1] = useState<number[]>([min, max]);
+    const [value, setValue] = useState<number[]>([min, max]);
 
-    const handleChange1 = (
+    const handleChange = (
         event: Event,
         newValue: number | number[],
         activeThumb: number,
@@ -25,37 +26,37 @@ export default function MultiSlider( {width, max=100, min=0} :Props ){
         }
 
         if (activeThumb === 0) {
-        setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+            setValue([Math.min(newValue[0], value[1] - minDistance), value[1]]);
         } else {
-        setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+            setValue([value[0], Math.max(newValue[1], value[0] + minDistance)]);
         }
     };
-    const MIN = 0;
-    const MAX = 100;
-    const marks= [
-        {
-            value: MIN,
-            label : '0세'
-        },
-        {
-            value: MAX,
-            label : '100세'
-        }
-    ]
 
     return(
-    <Box sx={{width : {width}}}>
-        <Slider
-            getAriaLabel={() => 'Minimum distance'}
-            value={value1}
-            onChange={handleChange1}
-            valueLabelDisplay="auto"
-            getAriaValueText={valuetext}
-            color = "#000"
-            step={10}
-            marks = {marks}
-            disableSwap
-        />
-    </Box>
+        <Container>
+            <Box>
+                <Slider
+                    getAriaLabel={() => 'Minimum distance'}
+                    value={value}
+                    onChange={handleChange}
+                    valueLabelDisplay="off"
+                    getAriaValueText={valuetext}
+                    color = "#000"
+                    step={10}
+                    disableSwap
+                />
+            </Box>
+        </Container>
     )
 }
+
+
+const Container = styled.div`
+    box-sizing : border-box;
+    display: flex; 
+    align-items: center;
+    justify-content: center;
+`;
+const Box = styled.div`
+    width: 90%;
+`;
