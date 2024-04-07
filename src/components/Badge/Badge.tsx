4de5e0393dbeed11301e5ee7329d6ff4e5153a33
HyeseongUm <5391nks@naver.com>
2024-04-07 21:35:31 +0900

    badge component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled"
+
+type Props = {
+    bold? : boolean,
+    gradient? : boolean,
+    txt : string
+}
+
+export const Badge = ({bold=false, gradient=false, txt}:Props)=>{
+    return(
+    <InfoBadge bold={bold} gradient={gradient}>
+        <Text bold={bold}>{txt}</Text>
+    </InfoBadge>
+)}
+
+const InfoBadge = styled.div<{bold:boolean, gradient:boolean}>`
+    width: fit-content;
+    height: ${({ bold }) => (bold ? "42px" : "30px")};
+    background-color: black;
+    border-radius: ${({ bold }) => (bold ? "16px" : "10px")};
+    background-image: ${({ gradient }) => (gradient && 'linear-gradient(#A463FF,#26C6DA)')}; 
+    padding: ${({ bold }) => (bold ? "9px 20px" : "5px 15PX")};;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `
+const Text = styled.p<{bold:boolean}>`
+    font-weight: ${({ bold }) => (bold ? 700 : 600)};
+    font-size: ${({ bold }) => (bold ? "16px" : "12px")};
+    color: white;
+`
