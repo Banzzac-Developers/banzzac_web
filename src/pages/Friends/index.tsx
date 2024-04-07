885a851835c2e76335ed77aa1b56a6d042776fbb
hyojinLeee-git <splato8899@gmail.com>
2024-04-07 23:42:20 +0900

    feat: 사용하지 않는 divider import 삭제

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -1,6 +1,4 @@
-import DividerDefault from "@components/divider/divider";
-
-import InputDefault from "@components/input/input";
+import InputDefault from "@components/Input/input";
 
 import Navigator from "@layouts/Navigator";
 import SquareHeader from "@layouts/SquareHeader";
@@ -12,8 +10,13 @@ export default function Friends() {
         <SquareHeader
           title="친구"
           headerIcons={["search", "friendAdd", "setting"]}
-        />    
-        <InputDefault title={"아이디"} id={"name"} placeholder="8~30자리 영문" width="100%"/>
+        />
+        <InputDefault
+          title={"아이디"}
+          id={"name"}
+          placeholder="8~30자리 영문"
+          width="100%"
+        />
       </Navigator>
     </>
   );
