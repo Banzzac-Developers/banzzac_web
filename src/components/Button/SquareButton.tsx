c1bc3923ce1155bc5f5a7fb01c201dad4cf5ccc3
hyojinLeee-git <splato8899@gmail.com>
2024-04-10 21:18:23 +0900

    feat: add button stories

--- src/components/Button/SquareButton.tsx
+++ src/components/Button/SquareButton.tsx
@@ -1,16 +1,29 @@
 import styled from "@emotion/styled";
 
 type Props = {
+  /**
+   * button txt
+   */
   title: string;
+  /**
+   * 채워진 버튼 여부
+   */
   fill: boolean;
+  /**
+   * 버튼 배경색
+   */
   backgroundColor: string;
 };
 
-export default function SquareButton({ title, fill, backgroundColor } : Props) {
-  return <StyledButton fill={fill} backgroundColor={backgroundColor}>{title}</StyledButton>;
+export default function SquareButton({ title, fill, backgroundColor }: Props) {
+  return (
+    <StyledButton fill={fill} backgroundColor={backgroundColor}>
+      {title}
+    </StyledButton>
+  );
 }
 
-const StyledButton = styled.button<{fill : boolean, backgroundColor : string}>`
+const StyledButton = styled.button<{ fill: boolean; backgroundColor: string }>`
   width: 110px;
   height: 50px;
   font-size: 16px;
@@ -19,7 +32,7 @@ const StyledButton = styled.button<{fill : boolean, backgroundColor : string}>`
   align-items: center;
   border-radius: 8px;
   border: 2px solid;
-  background-color: ${({ fill, backgroundColor }) => (fill ? backgroundColor : "#FFFFFF")};
-  color: ${({ fill,backgroundColor }) => (fill ? "#FFFFFF" :  backgroundColor)};
+  background-color: ${({ fill, backgroundColor }) =>
+    fill ? backgroundColor : "#FFFFFF"};
+  color: ${({ fill, backgroundColor }) => (fill ? "#FFFFFF" : backgroundColor)};
 `;
-
