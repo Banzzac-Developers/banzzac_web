8e66a26beec2366c1d7e31bdeaadc2fcd511712d
hyojinLeee-git <splato8899@gmail.com>
2024-04-11 23:14:19 +0900

    feat: mock handler, data 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,7 +1,9 @@
 import { HttpResponse, http } from "msw";
+import chatHandlers from "./chat";
 
 export default [
   http.get("/todo", () => {
     return HttpResponse.json(["eat", "drink", "sleep"]);
   }),
+  ...chatHandlers,
 ];
