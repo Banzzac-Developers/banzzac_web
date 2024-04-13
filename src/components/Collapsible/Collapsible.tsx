03a036dec6fcd56865b93bca56bedae54eac20eb
hyojinLeee-git <splato8899@gmail.com>
2024-04-13 22:53:54 +0900

    feat: Collapsible component 수정

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -2,22 +2,18 @@ import { useState } from "react";
 import styled from "@emotion/styled";
 import { SingleProfileImage } from "@components/ProfileImage/ProfileImage";
 import SvgSelector from "@components/Svg/SvgSelector";
-import DogProfileModal from "@containers/DogProfileModal";
 
 type DogProfile = {
   name: string;
   profileImg: string;
-  age: string;
-  weight: string;
-  gender: string;
-  neutered: boolean;
-  size: string;
-  breed: string;
-  personality: string;
-  activityLevel: string;
 };
 
-export default function Collapsible(dogProfile: DogProfile) {
+type Props = {
+  dogProfile: DogProfile;
+  contents: React.ReactElement;
+};
+
+export default function Collapsible({ dogProfile, contents }: Props) {
   const [isOpen, setOpen] = useState(false);
 
   const toggleOpen = () => {
@@ -26,45 +22,70 @@ export default function Collapsible(dogProfile: DogProfile) {
 
   return (
     <Container>
-      <ProfileHeader onClick={toggleOpen}>
-        <SingleProfileImage
-          img={dogProfile.profileImg}
-          size={24}
-          border={2}
-          borderColor="#212121"
-        />
-        {dogProfile.name}
-        {isOpen ? (
-          <SvgSelector
-            svg={"expandUp"}
-            width={24}
-            height={24}
-            stroke={"#212121"}
-          />
-        ) : (
-          <SvgSelector
-            svg={"expandDown"}
-            width={24}
-            height={24}
-            stroke={"#212121"}
-          />
-        )}
-      </ProfileHeader>
-      {isOpen && <DogProfileModal dogProfile={dogProfile} />}
+      {isOpen ? (
+        <ContentsContainer>
+          <button onClick={toggleOpen}>
+            <SvgSelector
+              svg={"expandUp"}
+              width={24}
+              height={24}
+              stroke={"#212121"}
+            />
+          </button>
+          <div>{contents}</div>
+        </ContentsContainer>
+      ) : (
+        <ProfileHeader>
+          <ProfileWrapper>
+            <SingleProfileImage
+              img={dogProfile.profileImg}
+              size={77}
+              border={2}
+              borderColor="#00000080"
+            />
+            {dogProfile.name}
+          </ProfileWrapper>
+          <button onClick={toggleOpen}>
+            <SvgSelector
+              svg={"expandDown"}
+              width={24}
+              height={24}
+              stroke={"#212121"}
+            />
+          </button>
+        </ProfileHeader>
+      )}
     </Container>
   );
 }
 
 const Container = styled.div`
   width: 100%;
-  font-weight: 500;
-  font-size: 14px;
-  margin-bottom: 10px;
+  position: relative;
 `;
 
 const ProfileHeader = styled.div`
   display: flex;
   align-items: center;
+  width: 100%;
+  justify-content: space-between;
   padding: 3px 6px;
   cursor: pointer;
+  padding: 18px 24px;
+`;
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
+
+const ContentsContainer = styled.div`
+  padding: 56px 24px 24px 24px;
+  button {
+    position: absolute;
+    top: 17px;
+    right: 24px;
+  }
 `;
