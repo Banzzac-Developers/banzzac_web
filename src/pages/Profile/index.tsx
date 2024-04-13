08d40f566b4df8f33b9e32d89698798f9c29553a
hyojinLeee-git <splato8899@gmail.com>
2024-04-13 20:30:01 +0900

    feat: add profile container

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,13 +2,28 @@ import Navigator from "@layouts/Navigator";
 import WalkingHistory from "@containers/Profile/WalkingHistory";
 import MyProfile from "@containers/Profile/Profile";
 import DividerDefault from "@components/Divider/divider";
+import useProfile from "@hooks/profile/useProfile";
+import Seperator from "@components/Seperator";
+import UserCharacteristic from "@containers/Profile/UserCharacteristic";
+import usePets from "@hooks/profile/usePets";
 
 export default function Profile() {
+  const { data: profile } = useProfile("email");
+  const { data: pets } = usePets("email");
+
+  if (!profile?.data || !pets?.data) return <div />;
+
   return (
     <>
-      <MyProfile />
-      <DividerDefault width={"90%"} />
-      <WalkingHistory />
+      <div style={{ padding: "26px 24px 60px 24px" }}>
+        <MyProfile profile={profile.data[0]} pets={pets.data} />
+        <Seperator height={15} />
+        <UserCharacteristic {...profile.data[0]} />
+        <Seperator height={40} />
+        <DividerDefault width={"100%"} />
+        <Seperator height={16} />
+        <WalkingHistory />
+      </div>
       <Navigator />
     </>
   );
