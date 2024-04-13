b85210e83f47b7e8824445c0db172826ad13e0b6
hyojinLeee-git <splato8899@gmail.com>
2024-04-13 16:52:53 +0900

    feat: add chatting list page

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,3 +1,4 @@
+import ChatContainer from "@containers/chat";
 import Navigator from "@layouts/Navigator";
 import SquareHeader from "@layouts/SquareHeader";
 
@@ -5,7 +6,7 @@ export default function Chat() {
   return (
     <>
       <SquareHeader title="채팅" headerIcons={["search", "setting"]} />
-
+      <ChatContainer />
       <Navigator />
     </>
   );
