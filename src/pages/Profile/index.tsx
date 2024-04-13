05307658b40b516a1b3c27dd891bf6f551a8947b
hyojinLeee-git <splato8899@gmail.com>
2024-04-13 17:47:56 +0900

    feat: divider default 수정

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,17 +1,15 @@
 import Navigator from "@layouts/Navigator";
 import WalkingHistory from "@containers/Profile/WalkingHistory";
 import MyProfile from "@containers/Profile/Profile";
-import DividerDefault from "@components/divider/divider";
-
+import DividerDefault from "@components/Divider/divider";
 
 export default function Profile() {
   return (
     <>
-    <MyProfile/>
-    <DividerDefault width={"90%"}/>
-    <WalkingHistory/>
-    <Navigator/>
-      
+      <MyProfile />
+      <DividerDefault width={"90%"} />
+      <WalkingHistory />
+      <Navigator />
     </>
   );
 }
