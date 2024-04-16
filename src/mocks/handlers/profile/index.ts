4594d1d38cb103bf74a321a5e00fdc4802576c47
hyojinLeee-git <splato8899@gmail.com>
2024-04-16 23:39:12 +0900

    refactor: mock handler에 이름 부여

--- src/mocks/handlers/profile/index.ts
+++ src/mocks/handlers/profile/index.ts
@@ -7,26 +7,40 @@ import {
   updateUserResponse,
 } from "./data";
 
+const fetchPets = http.get("/api/profile/dog/:id", () => {
+  return HttpResponse.json(petData);
+});
+
+const fetchProfile = http.get("/api/profile/:id", () => {
+  return HttpResponse.json(profile);
+});
+
+const deletePet = http.get("/api/profile/dog/:id/delete/:name", () => {
+  return HttpResponse.json(deletePetResponse);
+});
+
+const updatePet = http.post("/api/profile/dog/:id/:name", () => {
+  return HttpResponse.json(updatePetResponse);
+});
+
+const updateUser = http.post("/api/profile/:id", () => {
+  return HttpResponse.json(updateUserResponse);
+});
+
+const withdraw = http.post("/api/profile/:id/withdraw", () => {
+  return HttpResponse.json();
+});
+
+const addPet = http.post("/api/profile/dog/:id", () => {
+  return HttpResponse.json();
+});
+
 export default [
-  http.get("/api/profile/dog/:id", () => {
-    return HttpResponse.json(petData);
-  }),
-  http.get("/api/profile/:id", () => {
-    return HttpResponse.json(profile);
-  }),
-  http.get("/api/profile/dog/:id/delete/:name", () => {
-    return HttpResponse.json(deletePetResponse);
-  }),
-  http.post("/api/profile/dog/:id/:name", () => {
-    return HttpResponse.json(updatePetResponse);
-  }),
-  http.post("/api/profile/:id", () => {
-    return HttpResponse.json(updateUserResponse);
-  }),
-  http.post("/api/profile/:id/withdraw", () => {
-    return HttpResponse.json();
-  }),
-  http.post("/api/profile/dog/:id", () => {
-    return HttpResponse.json();
-  }),
+  fetchPets,
+  fetchProfile,
+  deletePet,
+  updatePet,
+  updateUser,
+  withdraw,
+  addPet,
 ];
