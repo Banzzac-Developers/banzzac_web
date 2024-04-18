bf2c21c3c2a67495d0c1dfcd4fbcb10102061152
hyojinLeee-git <splato8899@gmail.com>
2024-04-18 14:39:52 +0900

    fix: RoundButton 수정
    title에 react element 받을 수 있게 수정, onClick 추가

--- src/components/Button/RoundButton.tsx
+++ src/components/Button/RoundButton.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 type Props = {
-  title: string;
+  title: string | React.ReactElement;
   fill: boolean;
   /**
    * 버튼 배경색
@@ -11,15 +11,30 @@ type Props = {
   active?: boolean;
 };
 
-export default function RoundButton({ title, fill, backgroundColor }: Props) {
+export default function RoundButton({
+  title,
+  fill,
+  backgroundColor,
+  active = false,
+  onClick,
+}: Props) {
   return (
-    <StyledButton fill={fill} backgroundColor={backgroundColor}>
+    <StyledButton
+      onClick={onClick}
+      active={active}
+      fill={fill}
+      backgroundColor={backgroundColor}
+    >
       {title}
     </StyledButton>
   );
 }
 
-const StyledButton = styled.button<{ fill: boolean; backgroundColor: string }>`
+const StyledButton = styled.button<{
+  fill: boolean;
+  backgroundColor: string;
+  active: boolean;
+}>`
   width: 110px;
   height: 42px;
   font-size: 16px;
@@ -30,7 +45,7 @@ const StyledButton = styled.button<{ fill: boolean; backgroundColor: string }>`
   line-height: 24px;
   font-weight: 700;
   border-radius: 22px;
-  border: 2px solid;
+  border: ${({ active }) => (active ? "2px solid" : "1px solid")};
   background-color: ${({ fill, backgroundColor }) =>
     fill ? backgroundColor : "#FFFFFF"};
   color: ${({ fill, backgroundColor }) => (fill ? "#FFFFFF" : backgroundColor)};
